65ab663d4527b3a0900b1c41d8827764c366c7ad
Thariq Khalid <thariq.khalid@gmail.com>
2020-04-28 23:42:24 +0300

    Data loader without arranging the folders

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,14 @@
 import torch
 from torch.utils.data.dataset import Dataset
 from torchvision import datasets, transforms, models
+from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
 
 # THERE ARE MULTIPLE WAYS TO HAVE CUSTOM DATA LOADERS. WE'LL EXPLORE BOTH AND SEE WHICH IS GOOD FOR US
 
 DATA_DIR = "dataset-dist/phase-01/training/"
 
+'''
 class FakeImagesDataset(Dataset):
     def __init__(self):
         self.transformations = transforms.Compose([transforms.CenterCrop(100),
@@ -19,12 +22,30 @@ class FakeImagesDataset(Dataset):
 
     def __len__(self):
         return count
+'''
 
 
-def load_split_train_test(DATA_DIR, valid_size = 0.2):
+def load_split_train_test(data_dir, valid_size = 0.2):
     train_transforms = transforms.Compose([transforms.Resize(224),
                                            transforms.ToTensor()])
     test_transforms = transforms.Compose([transforms.Resize(224),
                                            transforms.ToTensor()])
-    train_data =  datasets.ImageFolder(DATA_DIR, transform=train_transforms)
-    test_data = datasets.ImageFolder(DATA_DIR, transform=test_transforms)
+    train_data =  datasets.ImageFolder(data_dir, transform=train_transforms)
+    test_data = datasets.ImageFolder(data_dir, transform=test_transforms)
+
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+    np.random.shuffle(indices)
+    train_idx, test_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+    trainloader = torch.utils.data.DataLoader(train_data,
+                                              sampler=train_sampler, batch_size=4)
+    testloader = torch.utils.data.DataLoader(test_data,
+                                             sampler=test_sampler, batch_size=4)
+    return trainloader, testloader
+
+
+trainloader, testloader = load_split_train_test(DATA_DIR, .2)
+print(trainloader.dataset.classes)
